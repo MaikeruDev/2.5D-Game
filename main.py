92ccd6922c66b70257f5a32bd8023a0da6a50fb3
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2023-08-24 15:50:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,46 @@
 import config
 from player import Player
+from player import Collectible
+from player import Item
+import player as playerpy
 from raycaster import Raycaster
 import raylibpy as rl 
 from config import MAP
-from minimap import MiniMap 
-import raycaster
+from minimap import MiniMap  
 
 def main():
-    rl.init_window(config.SCREEN_WIDTH, config.SCREEN_HEIGHT, "Pixel Gallery")
+
+    rl.init_window(config.SCREEN_WIDTH, config.SCREEN_HEIGHT, "2.5 D")
     rl.set_target_fps(config.FPS) 
 
     player = Player(2, 2)
-
     raycaster = Raycaster(player)
-    
     mini_map = MiniMap(player)
 
+    sword = Item("Sword")
+    potion = Item("Health Potion")
 
-    while not rl.window_should_close():
+    # Add items to player's inventory
+    player.add_item_to_inventory(sword)
+    player.add_item_to_inventory(potion)
+ 
+    while not rl.window_should_close(): 
+
         player.handle_input(MAP)
         raycaster.cast_rays()
-         
-        rl.draw_text("Mini Map", 10, 70, 20, rl.DARKGRAY)
+
+        rl.draw_text("Mini Map", 10, 110, 20, rl.DARKGRAY)
         draw_mini_map(player)
         rl.draw_fps(config.SCREEN_WIDTH - 100, 10) 
 
         rl.begin_drawing()
         rl.clear_background(rl.RAYWHITE) 
-        
+
+        player.draw()
+
+        if player.show_inventory:
+            playerpy.draw_inventory(player)
+
         rl.end_drawing()
 
     rl.close_window()
@@ -45,7 +58,6 @@ def draw_mini_map(player):
     player_map_x = int(player.x * map_scale) + map_x
     player_map_y = int(player.y * map_scale) + map_y
     rl.draw_circle(player_map_x, player_map_y, 3, rl.RED) 
-
 
 if __name__ == "__main__":
     main()
