6ac02fd7c3abd1d3565656d597eb2858072d1097
Maikeru <78266557+MaikeruDev@users.noreply.github.com>
2023-08-24 13:16:32 +0200

    Update player.py
    fixed collision bug

--- player.py
+++ player.py
@@ -5,50 +5,56 @@ class Player:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.rotation = 0  # Angle at which the player is facing, initially set to 0.
-        self.move_speed = 0.05  # Speed of movement forward/backward.
-        self.rotation_speed = 0.05  # Speed of rotation.
-        self.dirX = 1
-        self.dirY = 0
-        self.planeX = 0 
-        self.planeY = 0.66   
-        self.rotation_angle = 0  # Angle in radians
+        self.rotation = 0.0
+        self.move_speed = 0.05
+        self.rotation_speed = 0.05
+        self.dirX = 1.0
+        self.dirY = 0.0
+        self.planeX = 0.0
+        self.planeY = 0.66
+
+    def is_valid_position(self, world_map, x, y):
+        map_height = len(world_map)
+        map_width = len(world_map[0])
+
+        map_x = int(x)
+        map_y = int(y)
+
+        if map_x < 0 or map_x >= map_width or map_y < 0 or map_y >= map_height:
+            return False
+
+        return world_map[map_y][map_x] == 0
 
     def handle_input(self, world_map):
         if rl.is_key_down(rl.KEY_W):
             new_x = self.x + self.dirX * self.move_speed
-            new_y = self.y + self.dirY * self.move_speed
-            
-            if world_map[int(new_x)][int(self.y)] == 0:
+            new_y = self.y + self.dirY * self.move_speed 
+            if self.is_valid_position(world_map, new_x, new_y):
                 self.x = new_x
-            if world_map[int(self.x)][int(new_y)] == 0:
                 self.y = new_y
 
         if rl.is_key_down(rl.KEY_S):
             new_x = self.x - self.dirX * self.move_speed
             new_y = self.y - self.dirY * self.move_speed
-            
-            if world_map[int(new_x)][int(self.y)] == 0:
+            if (
+                world_map[int(new_x)][int(self.y)] == 0
+                and world_map[int(self.x)][int(new_y)] == 0
+            ):
                 self.x = new_x
-            if world_map[int(self.x)][int(new_y)] == 0:
                 self.y = new_y
 
-        if rl.is_key_down(rl.KEY_D):
-            # Turn left.
-            self.rotation_angle += self.rotation_speed
-            if self.rotation_angle >= 2 * math.pi:
-                self.rotation_angle -= 2 * math.pi
-
         if rl.is_key_down(rl.KEY_A):
-            # Turn right.
-            self.rotation_angle -= self.rotation_speed
-            if self.rotation_angle < 0:
-                self.rotation_angle += 2 * math.pi
+            oldDirX = self.dirX
+            self.dirX = self.dirX * math.cos(self.rotation_speed) - self.dirY * math.sin(self.rotation_speed)
+            self.dirY = oldDirX * math.sin(self.rotation_speed) + self.dirY * math.cos(self.rotation_speed)
+            oldPlaneX = self.planeX
+            self.planeX = self.planeX * math.cos(self.rotation_speed) - self.planeY * math.sin(self.rotation_speed)
+            self.planeY = oldPlaneX * math.sin(self.rotation_speed) + self.planeY * math.cos(self.rotation_speed)
 
-        # Update direction and camera plane vectors based on rotation_angle
-        self.dirX = math.cos(self.rotation_angle)
-        self.dirY = math.sin(self.rotation_angle)
-
-        # The camera plane is perpendicular to the direction vector
-        self.planeX = -self.dirY
-        self.planeY = self.dirX
+        if rl.is_key_down(rl.KEY_D):
+            oldDirX = self.dirX
+            self.dirX = self.dirX * math.cos(-self.rotation_speed) - self.dirY * math.sin(-self.rotation_speed)
+            self.dirY = oldDirX * math.sin(-self.rotation_speed) + self.dirY * math.cos(-self.rotation_speed)
+            oldPlaneX = self.planeX
+            self.planeX = self.planeX * math.cos(-self.rotation_speed) - self.planeY * math.sin(-self.rotation_speed)
+            self.planeY = oldPlaneX * math.sin(-self.rotation_speed) + self.planeY * math.cos(-self.rotation_speed)
